4b37df3f3e45296ae92edef5af1a6937efc927db
amirhosseinhosseinii1991-prog <amirhosseinhosseinii1991@gmail.com>
2026-07-23 02:58:10 +0330

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,5 @@ def api_prices():
     return jsonify(results)
 
 if __name__ == "__main__":
-    # این بخش حیاتی برای Render است
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
